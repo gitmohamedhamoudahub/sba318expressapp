43603b26003cd9c9f904c927509f9f0bfb84d5f6
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-01-21 14:34:26 -0500

    GET Company added

---
+++ data/companies.js
@@ -0,0 +1,57 @@
+
+
+const companies = [
+    {
+    "id": 1,
+    "Address": "123 Main Street, City, Country",
+    "Phone1": "+1234567890",
+    "Phone2": "+0987654321",
+    "Website": "https://www.example.com",
+    "isConsultant": true, // true or false
+    "isSellerRetail": true, // true or false
+    "isSellerWholesale": false, // true or false
+    "hasInternationalShipping": true, // true or false
+    "hasLocalShipping": true, // true or false
+    "isVerifiedBusiness": false, // true or false
+    "images": [
+        "uploads/image1.jpg", // Path to image 1
+        "uploads/image2.jpg"  // Path to image 2
+    ]
+},
+{
+    "id": 2,
+    "Address": "123 Main Street, City, Country",
+    "Phone1": "+1234567890",
+    "Phone2": "+0987654321",
+    "Website": "https://www.example.com",
+    "isConsultant": true, // true or false
+    "isSellerRetail": true, // true or false
+    "isSellerWholesale": false, // true or false
+    "hasInternationalShipping": true, // true or false
+    "hasLocalShipping": true, // true or false
+    "isVerifiedBusiness": false, // true or false
+    "images": [
+        "uploads/image1.jpg", // Path to image 1
+        "uploads/image2.jpg"  // Path to image 2
+    ]
+},
+{
+    "id": 3,
+    "Address": "123 Main Street, City, Country",
+    "Phone1": "+1234567890",
+    "Phone2": "+0987654321",
+    "Website": "https://www.example.com",
+    "isConsultant": true, // true or false
+    "isSellerRetail": true, // true or false
+    "isSellerWholesale": false, // true or false
+    "hasInternationalShipping": true, // true or false
+    "hasLocalShipping": true, // true or false
+    "isVerifiedBusiness": false, // true or false
+    "images": [
+        "uploads/image1.jpg", // Path to image 1
+        "uploads/image2.jpg"  // Path to image 2
+    ]
+}
+    ];
+
+    module.exports = companies;
--- service.js
+++ service.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 PORT = 3333;
 
+const allCompanies = require('./data/companies');
 
 
+app.get('/api/v1',(request,response)=>{
+    response.status(200).send('Root is here!');
+});
 
+app.get('/api/v1/companies', (request,response) => {
+    response.status(200).send(allCompanies);    
+});
+
+app.get('/api/v1/companies/:companyID', (request,response) => {
+    const company = allCompanies.filter(company => company.id == request.params.companyID)
+    response.status(200).send(company);     
+});
 
 
 
