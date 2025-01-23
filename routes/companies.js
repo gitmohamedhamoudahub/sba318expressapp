41455be44bc10ff031a59031ab0b9ab646831aef
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-01-23 09:31:23 -0500

    admin delete route

--- routes/companies.js
+++ routes/companies.js
@@ -12,22 +12,47 @@ router.get('/api/v1/admin/companies', (request,response) => {
 });
 
 router.get('/api/v1/admin/companies/:companyID', (request,response) => {
+    const {body,
+        params: { companyID }
+    } = request;
+    const myID = parseInt(companyID);
+    if(isNaN(myID)) return response.sendStatus(400);
+    const findCompanyIndex = allCompanies.find(company => company.id === myID);
+    if(findCompanyIndex === -1) return response.sendStatus(404); 
+
     const company = allCompanies.filter(company => company.id == request.params.companyID)
     response.status(200).send(company);     
 });
 
 router.put('/api/v1/admin/companies/:companyID', (request , response) => {
-    //const company = allCompanies.delete(company => company.id == request.params.companyID)
-    // response.send('Deleted company ' + company.id + '-' + company.name );     
+    const {body,
+        params: { companyID }
+    } = request;
+    const myID = parseInt(companyID);
+    if(isNaN(myID)) return response.sendStatus(400);
+   console.log(myID);
+    const findCompanyIndex = allCompanies.findIndex(company => company.id === myID);
+    if(findCompanyIndex === -1) return response.sendStatus(404); 
+    console.log(findCompanyIndex);
+    allCompanies.splice(findCompanyIndex, 1);
+    
     console.log('Update company ');
     response.status(200).send('');
 });
 
 router.delete('/api/v1/admin/companies/:companyID', (request , response) => {
-    //const company = allCompanies.delete(company => company.id == request.params.companyID)
-    // response.send('Deleted company ' + company.id + '-' + company.name );     
+    const {body,
+        params: { companyID }
+    } = request;
+    const myID = parseInt(companyID);
+    if(isNaN(myID)) return response.sendStatus(400);
+   console.log(myID);
+    const findCompanyIndex = allCompanies.findIndex(company => company.id === myID);
+    if(findCompanyIndex === -1) return response.sendStatus(404); 
+    console.log(findCompanyIndex);
+    allCompanies.splice(findCompanyIndex, 1); 
     console.log('Delete company ');
-    response.status(200).send('');
+    response.status(200).send(allCompanies);
 });
 
 module.exports = router;
